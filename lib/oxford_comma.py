ffede1ea23c05d83eab0e4e8c3ea3237b075b735
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-12-14 14:00:41 -0500

    final product

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return ' and '.join(items)
+    else: oxford = ', '.join(items[:-1]) + ', and ' + items[-1]
+    return oxford
+
+print(oxford_comma(["fiddleheads", "okra", "kohlrabi"]))
